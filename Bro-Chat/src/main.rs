aa6ba1d4123da4ddade5b5851125257c0c2b3423
Rakotoarilala51 <maitreaina51@gmail.com>
2025-05-01 16:58:44 +0300

    spawning task

--- Bro-Chat/src/main.rs
+++ Bro-Chat/src/main.rs
@@ -4,14 +4,22 @@ use tokio::{
 };
 #[tokio::main]
 async fn main() {
-    let listner = TcpListener::bind("localhost::8080").await.unwrap();
-    let (mut socket, _address) = listner.accept().await.unwrap();
-    let (stream_reader, mut stream_writer) = socket.split();
-    let mut message = String::new();
-    let mut reader = BufReader::new(stream_reader);
+    let listner = TcpListener::bind("localhost:8080").await.unwrap();
     loop {
-        reader.read_line(&mut message).await.unwrap();
-        stream_writer.write_all(message.as_bytes()).await.unwrap();
-    }
+        let (mut socket, _address) = listner.accept().await.unwrap();
+        tokio::spawn(async move{
+            let (stream_reader, mut stream_writer) = socket.split();
+            let mut message = String::new();
+            let mut reader = BufReader::new(stream_reader);
+            loop {
+                let bytes_read = reader.read_line(&mut message).await.unwrap();
+                if bytes_read == 0{
+                    break;
+                }
+                stream_writer.write_all(message.as_bytes()).await.unwrap();
+                message.clear();
+            }
+        });
 
+    }
 }
