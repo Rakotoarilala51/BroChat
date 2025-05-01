b333c566e38a2caf44d49ec28c47c238b8204e7c
Rakotoarilala51 <maitreaina51@gmail.com>
2025-05-01 14:57:14 +0300

    building the echo server

--- Bro-Chat/src/main.rs
+++ Bro-Chat/src/main.rs
@@ -1,4 +1,17 @@
+use tokio::{
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+    net::TcpListener
+};
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let listner = TcpListener::bind("localhost::8080").await.unwrap();
+    let (mut socket, _address) = listner.accept().await.unwrap();
+    let (stream_reader, mut stream_writer) = socket.split();
+    let mut message = String::new();
+    let mut reader = BufReader::new(stream_reader);
+    loop {
+        reader.read_line(&mut message).await.unwrap();
+        stream_writer.write_all(message.as_bytes()).await.unwrap();
+    }
+
 }
